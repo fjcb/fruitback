01789e2fb4352541f67087d3b06f6e62ceaf4f36
RobertKuhfss <kuhfss@informatik.uni-bremen.de>
2013-02-19 17:40:19 +0100

    Initiated FruitBack project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fruitback::Application.config.secret_token = 'cbc7a356bd0792ed1150b354c0c3051c9d3756ddbdb7c2ed01ddd779808a006a906968029a2c3b9d0684ce4f3ca4169f3a823ab8dd21e5412d45266c81db400d'
