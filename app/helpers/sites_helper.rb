43bf5d095a851584206c66c36018b28ad99d0d55
fjcb <fritz@jacob-meyer.de>
2013-02-22 10:21:35 +0100

    Added widget button_position helper.

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,2 +1,31 @@
 module SitesHelper
+  
+  def button_position id
+    
+    x = 100
+    y = 50
+    offset = 10
+    half_x = x / 2;
+    
+    if(id == 0)
+      "top: -#{offset}px; left: #{offset}px; width: #{x}px; height: #{y}px;"
+    elsif(id == 1)
+      "top: -#{offset}px; left: 50%; margin-left: -#{half_x}px; width: #{x}px; height: #{y}px;"
+    elsif(id == 2)
+      "top: -#{offset}px; right: #{offset}px; width: #{x}px; height: #{y}px;"
+    elsif(id == 3)
+      "left: -#{offset}px; top: 40%; width: #{x}px; height: #{y}px;"
+    elsif(id == 4)
+      "right: -#{offset}px; top: 40%; width: #{y}px; height: #{x}px;"
+    elsif(id == 5)
+      "bottom: -#{offset}px; left: #{offset}px; width: #{x}px; height: #{y}px;"
+    elsif(id == 6)
+      "bottom: -#{offset}px; left: 50%; margin-left: -#{half_x}px; width: #{x}px; height: #{y}px;"
+    elsif(id == 7)  
+      "bottom: -#{offset}px; right: #{offset}px; width: #{x}px; height: #{y}px;"
+    else
+      ""
+    end
+  end
+  
 end
