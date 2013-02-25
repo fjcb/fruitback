f6d453b7b3adb3ee2d6733100d99941d7c8dcacf
RobertKuhfss <kuhfss@informatik.uni-bremen.de>
2013-02-25 16:35:51 +0100

    Moved widget.js, edit customer site works now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   inherit_resources
   load_and_authorize_resource except: :check
   
+  layout 'customer_site'
+  
   def check
     user = User.find_by_email(params[:email])
     render json: !!user
