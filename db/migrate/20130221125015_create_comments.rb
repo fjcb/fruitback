092882ef3eb6a98c5e15770fc6d22231562e2a51
RobertKuhfss <kuhfss@informatik.uni-bremen.de>
2013-03-02 19:30:58 +0100

    Scrumbled eggs, made string to text

--- db/migrate/20130221125015_create_comments.rb
+++ db/migrate/20130221125015_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     
     create_table :comments do |t|
-      t.string :text, :null => false
+      t.text :text, :null => false
 
       t.timestamps null: false
       
