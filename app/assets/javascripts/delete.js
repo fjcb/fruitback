26e078a112ed61d89b73ef36d4daaaff17b525d2
RobertKuhfss <kuhfss@informatik.uni-bremen.de>
2013-03-02 18:18:36 +0100

    Added ajax support for deletion of comments

--- app/assets/javascripts/delete.js
+++ app/assets/javascripts/delete.js
@@ -1,19 +1,39 @@
 
-$('.delete').click(function(){
+$('.delete').click(function() {
+	$this = $(this);
 	
-	if($(this).val() == "Delete") {
+	if($this.val() == "Delete") {
 		// reset other buttons
 		$('.delete')
 			.val("Delete")
 			.removeClass('btn-primary')
 			.addClass('btn-danger');
 		
-		$(this)
+		$this
 			.val("Are you sure?")
 			.removeClass('btn-danger')
 			.addClass('btn-primary');
 		
-		return false;
+	} else {
+		if(!$this.hasClass('ajax'))
+			return true
+		
+		$this.val("Deleting...")
+		
+		$.ajax({
+			type: "delete",
+			dataType: 'json',
+			url: $this.closest('form').attr('action'),
+			success: function(data) {
+				if(data.success) {
+					$this.closest('.posting').fadeOut();
+				}
+			},
+			error: function() {
+				$this.val("Sorry, an error occured.")
+			}
+		});
 	}
 	
-})
+	return false;
+});
--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -5,7 +5,6 @@ $("#vote").on("click", "button", function(){
 	$this = $(this);
 	$.ajax({
 		url: $this.parent().data("uri"), 
-		method: "POST",
 		dataType: "json",
 		success: function(data) {
 			$("#score").html(data.score);
